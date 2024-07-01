194f0bae9a5beb879e498469d08f374ce29d2c61
Hélio Fernandes Sebastião <155336173+helfese@users.noreply.github.com>
2024-07-01 02:47:01 +0100

    Add intro

--- k-means.asm
+++ k-means.asm
@@ -1,3 +1,45 @@
+# Variaveis em memoria
+.data
+
+    # Input A 
+     #n_points:    .word 9
+     #points:      .word 0,0, 1,1, 2,2, 3,3, 4,4, 5,5, 6,6, 7,7, 8,8
+
+    # Input B
+     #n_points:    .word 5
+     #points:      .word 4,2, 5,1, 5,2, 5,3, 6,2
+
+    # Input C
+    n_points:    .word 23
+    points:      .word 0,0, 0,1, 0,2, 1,0, 1,1, 1,2, 1,3, 2,0, 2,1, 5,3, 6,2, 6,3, 6,4, 7,2, 7,3, 6,8, 6,9, 7,8, 8,7, 8,8, 8,9, 9,7, 9,8
+
+    # Input D
+    #n_points:    .word 30
+    #points:      .word 16,1, 14,2, 18,6, 20,3, 21,1, 17,4, 21,7, 16,4, 21,6, 19,6, 4,24, 6,24, 8,23, 6,26, 6,26, 6,23, 8,25, 7,26, 7,20, 4,21, 4,10, 2,10, 3,11, 2,12, 4,13, 4,9, 4,9, 3,8, 0,10, 4,10
+
+    # Input E
+    # n_points:    .word 35
+    # points:      .word 16,1, 14,2, 18,6, 20,3, 21,1, 17,4, 21,7, 16,4, 21,6, 19,6, 4,24, 6,24, 8,23, 6,26, 6,26, 6,23, 8,25, 7,26, 7,20, 4,21, 4,10, 2,10, 3,11, 2,12, 4,13, 4,9, 4,9, 3,8, 0,10, 4,10, 20,22, 21,27, 22,25, 25,29, 28,21
+
+    # Valores de centroids e k para mainSingleCluster:
+    # centroids:   .word 0,10
+    # k:           .word 1
+
+    # Valores de centroids, k e L para mainKMeans:
+    centroids:   .word 0,0, 10,0, 0,10
+    k:           .word 3
+    L:           .word 120
+    oldCentroids:.word 0,0, 10,0, 0,10      # Igual a centroids, sera depois atualizado
+
+    clusters:    .zero 4*23                 # 4* n_points
+    
+    # Definicoes de cores
+    colors:      .word 0xff0000, 0x00ff00, 0x0000ff
+    .equ RED,   0xFF0000
+    .equ WHITE, 0xFFFFFF
+    .equ BLACK, 0x000000
+
+# Codigo
 .text
 
     # Inicializa k
